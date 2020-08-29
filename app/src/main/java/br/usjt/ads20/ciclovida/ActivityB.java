4e199487a9034ee08d84a6dc9a1da112a356af21
Sergio Bonato <asbonato@gmail.com>
2020-08-28 23:54:32 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ads20/ciclovida/ActivityB.java
@@ -0,0 +1,15 @@
+package br.usjt.ads20.ciclovida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityB extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
---
+++ app/src/main/java/br/usjt/ads20/ciclovida/ActivityC.java
@@ -0,0 +1,15 @@
+package br.usjt.ads20.ciclovida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityC extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
